9f283967f618c00a89f8be46538253bb5f314b02
Amjad <84246684+amjadcp@users.noreply.github.com>
2021-06-03 13:13:55 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,17 +1,101 @@
+'''
+Author : Amjad
+Date   : 3 JUN 2021 11.30 PM
+About  : Hang man game using PYTHON
+'''
+
+#IMPORTED PYTHON LIBRARIES
 import random
 import os
 import time
+import word_list
+#from colorama import Fore
 
+def g_m(i):
+    if i==2:
+        g_2()
+    elif i==3:
+        g_3()
+    elif i==4:
+        g_4()
+    else:
+        g_p()
+    
+
+def g_1():
+    print(f'''--------------
+|            |            
+|            o
+|
+|
+|
+|
+|
+|''')
+
+def g_2():
+     print(f'''--------------
+|            |            
+|            0
+|           
+|            
+|           
+|
+|
+|''')
+
+def g_3():
+     print(f'''--------------
+|            |            
+|            0
+|           /|\\
+|            
+|           
+|
+|
+|''')
+
+def g_4():
+     print(f'''--------------
+|            |            
+|            0
+|           /|\\
+|            |
+|           / \\
+|
+|
+|''')
+
+def g_p():
+     print(f'''--------------
+|            |            
+|            o
+|           
+|            
+|            0
+|           /|\\
+|            |
+|           / \\''')
+
+
+
+
+
+#A FUNCTION TO PRINT HEADING WITH SHOWING ATTEMPT
 def head(i):
-    print(f'''---------------------------------
-                        HANG MAN
+    print(f'''------------------------------------------------
+    -
+                    HANG MAN
+
 --------------- ATTEMPT = {i} of 4-----------------\n\n''')
 
-def main(word, limit):
+# a function to check the answer
+def answer(word, limit):
     i = 0
     tem = []
     temp = ""
-    guess = input("\nGuess the word : ")
+    Guess = input("Guess the word : ")
+    guess = Guess.upper()
     for char in guess:
         if char in word:
             tem.insert(i, char)
@@ -20,46 +104,62 @@ def main(word, limit):
         i = i+1
     
     return temp.join(tem)
-'''
-def update(temp, limit):
-    i = 0
-    str = []
-    store = ""
-    for char in temp:
-        if char in '''
-    
     
 
 
-head(0)
-words = ["banana"]
-word = random.choice(words)
+head(1)
+g_1()
+words = ["apple", "banana", "pappaya", "car", "bike"]
+Word = random.choice(words)
+word = Word.upper()
 limit = len(word)
-print(" _ " * limit)
+print("\n\n", " _ " * limit, "\n\n")
 i = 1
-result = ""
+result = False
+
 while 5>i:
     if i-1 != 0:
       head(i)
-    temp = main(word, limit)
+      
+    
+    temp = answer(word, limit)
     ans = ''
+    
     for ch in temp:
       ans = ans + ch + ' '
-    print("\n", ans[:-1], "\n")
+   
 
     temp_size = len(temp)
+    
     if limit<temp_size:
         print(f"""Breaked limit (only {limit} letters needed""")
+        g_m(i)
+
     elif limit>temp_size:
         print(f"""{limit} letters needed""")
+        g_m(i)
+
+    elif limit==temp==word:
+        g_p()
+        result = True
+        print("YOU ARE MY LIFE SAVER")
+        print(ans)
+    elif limit==temp:
+         print(f'''KEEP GOING''')
+    
     else:
-        result = "YOU WINS"
-        print(result)
-        print(word)
-    i = i+1
-    if result != "":
-        break
-    time.sleep(1)
+        print(f'''KEEP GOING       {limit} letters needed''')
+        g_m(i)
+        
+
+    if result==True:
+        break   
+   
+    time.sleep(5)
     os.system("cls") # in ubundu cls -> clear
+    print(f"""HINT : {limit} letters needed""")
+    i = i+1
+
+print("NIRTTHI VERAVALLA PANIKKUM PODO")
     
 
